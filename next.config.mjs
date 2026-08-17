f0b98290186d5bee00942f584c11dc52ab1611a7
Shree Charan N <shreecharan5277443@gmail.com>
2026-08-18 00:10:56 +0530

    perf: next.config - trailing slash, compress, remove powered-by header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  compress: true,
+  poweredByHeader: false,
   images: {
     unoptimized: true,
   },
